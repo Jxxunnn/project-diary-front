2f15ab8ca4c372605ea7d6714b9df490e893d23a
Jxxunnn <roof2586@naver.com>
2023-02-02 17:50:12 +0900

    set global style 적용

---
+++ src/styles/GlobalStyle.tsx
@@ -0,0 +1,29 @@
+import reset from "styled-reset";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`    
+  ${reset}   
+  html {
+    font-family : "nanum";
+    font-size: 62.5%;
+  }
+  body {
+    background-color: #f6f6f6
+  }
+  #root {
+    min-height: 100vh;
+    margin:0 auto;
+    padding:0 2rem;   
+    background-color: white;
+    box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+
+    @media screen and (min-width: 650px) {
+      width: 64rem;
+    }
+    @media screen and (max-width: 650px) {
+      width: 90vw;
+    }
+  }    
+`;
+
+export default GlobalStyle;

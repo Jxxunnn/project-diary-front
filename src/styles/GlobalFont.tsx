09b88d233d4a77c3ea9b4f5e7ff6f1524acaaac3
Jxxunnn <roof2586@naver.com>
2023-02-02 17:50:01 +0900

    feat global font 적용

---
+++ src/styles/GlobalFont.tsx
@@ -0,0 +1,19 @@
+import reset from "styled-reset";
+import { createGlobalStyle } from "styled-components";
+import NanumPenRegular from "../assets/fonts/NanumPenScript-Regular.woff";
+import YeonSungRegular from "../assets/fonts/YeonSung-Regular.woff";
+
+const GlobalFont = createGlobalStyle`    
+     @font-face {
+        font-family: "nanum";
+        src: local("nanum"), url(${NanumPenRegular}) format('woff'); 
+        font-weight: normal;
+    }
+    @font-face {
+        font-family: "yeonsung";
+        src: local("yeonsung"), url(${YeonSungRegular}) format('woff');
+        font-weight: normal;
+    }
+`;
+
+export default GlobalFont;

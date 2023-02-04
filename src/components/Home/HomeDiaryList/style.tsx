0b981d300c16fcf5a026938134c087f0d8b60f0e
Jxxunnn <roof2586@naver.com>
2023-02-04 15:26:20 +0900

    style HomeDiaryList 컴포넌트 스타일링

---
+++ src/components/Home/HomeDiaryList/style.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const ControllerContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 2rem;
+  margin-bottom: 3rem;
+`;
+
+export const LeftContainer = styled.div``;
+
+export const RightContainer = styled.div`
+  flex-grow: 1;
+`;
+
+export const DiaryList = styled.ul``;

ad6f6535b2fdf1326ae247f31ffc5d1c4cd1f208
Jxxunnn <roof2586@naver.com>
2023-02-04 15:25:39 +0900

    feat HomeDiaryList 컴포넌트 구현

---
+++ src/components/Home/HomeDiaryList/index.tsx
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router";
+import HomeController from "../HomeController";
+import Button from "../../Common/Button";
+import * as S from "./style";
+import { DiaryItemType } from "../../../types/diary/diary.type";
+import HomeDiaryItem from "../HomeDiaryItem.tsx";
+
+type PropsType = {
+  diaryList: DiaryItemType[];
+};
+type CompareType = (a: DiaryItemType, b: DiaryItemType) => number;
+
+const DiaryList = ({ diaryList }: PropsType) => {
+  const [sortType, setSortType] = useState("latest");
+  const [filter, setFilter] = useState("all");
+  const navigate = useNavigate();
+
+  const sortOptionList = [
+    { value: "latest", name: "최신순" },
+    { value: "oldest", name: "지난순" },
+  ];
+  const filterOptionList = [
+    { value: "all", name: "전부다" },
+    { value: "good", name: "좋은 감정만" },
+    { value: "bad", name: "안좋은 감정만" },
+  ];
+
+  const getProcessdDiaryList = () => {
+    const compareByDate: CompareType = (a, b) => {
+      if (sortType === "latest") return b.date - a.date;
+      else return a.date - b.date;
+    };
+    const filterCallBack = (diary: DiaryItemType) => {
+      if (filter === "good") {
+        return diary.emotion < 3;
+      } else return diary.emotion >= 3;
+    };
+
+    const filteredList =
+      filter === "all"
+        ? [...diaryList]
+        : [...diaryList].filter((it) => filterCallBack(it));
+
+    return filteredList.sort(compareByDate);
+  };
+
+  return (
+    <div>
+      <S.ControllerContainer>
+        <S.LeftContainer>
+          <HomeController
+            value={sortType}
+            onChange={setSortType}
+            optionList={sortOptionList}
+          />
+          <HomeController
+            value={filter}
+            onChange={setFilter}
+            optionList={filterOptionList}
+          />
+        </S.LeftContainer>
+        <S.RightContainer>
+          <Button
+            variant="positive"
+            text="새 일기 쓰기"
+            onClick={() => navigate("/new")}
+          />
+        </S.RightContainer>
+      </S.ControllerContainer>
+      <S.DiaryList>
+        {getProcessdDiaryList().map((diary) => (
+          <HomeDiaryItem key={diary.id} {...diary} />
+        ))}
+      </S.DiaryList>
+    </div>
+  );
+};
+
+export default DiaryList;

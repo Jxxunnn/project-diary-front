6c25e70483631db48933e51a5672fb3132149414
Jxxunnn <roof2586@naver.com>
2023-02-04 15:25:22 +0900

    style Home 페이지 컨트롤 바 스타일링

---
+++ src/components/Home/HomeController/style.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Select = styled.select`
+  margin-right: 1rem;
+  border: none;
+  border-radius: 0.5rem;
+  padding: 1rem 2rem 1rem 2rem;
+  background: #ececec;
+  cursor: pointer;
+  font-family: "nanum";
+  font-size: 1.8rem;
+`;
+
+export const Option = styled.option``;

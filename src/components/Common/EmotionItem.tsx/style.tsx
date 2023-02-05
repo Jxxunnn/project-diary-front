7d4c8e4a39e3c8b3c56d0103d80398577f182565
Jxxunnn <roof2586@naver.com>
2023-02-05 22:17:47 +0900

    style EmotionItem 컴포넌트 스타일링

---
+++ src/components/Common/EmotionItem.tsx/style.tsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+import { palette } from "../../../styles/pallete";
+
+const colors = ["green", "lightGreen", "yellow", "orange", "red"] as const;
+
+export const EmotionItem = styled.li<{ isOn: boolean; variant: number }>`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-radius: 0.5rem;
+  padding: 2rem 0;
+  background-color: ${({ isOn, variant }) =>
+    isOn ? palette[colors[variant - 1]] : "#ececec"};
+  color: ${({ isOn }) => (isOn ? "white" : "black")};
+  cursor: pointer;
+`;
+
+export const EmotionImage = styled.img`
+  width: 50%;
+  margin-bottom: 1rem;
+`;
+
+export const EmotionText = styled.span`
+  font-size: 1.8rem;
+`;

a360490d943e3106ead8f10a8828c673a9f057c3
Jxxunnn <roof2586@naver.com>
2023-02-05 22:13:57 +0900

    feat 라우팅 404 exception 처리

--- src/components/Router/index.tsx
+++ src/components/Router/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 
 import HomePage from "../../pages/Home";
 import DiaryPage from "../../pages/Diary";
@@ -10,8 +10,9 @@ const Router = () => (
   <Routes>
     <Route path="/" element={<HomePage />} />
     <Route path="/new" element={<NewPage />} />
-    <Route path="/edit" element={<EditPage />} />
+    <Route path="/edit/:id" element={<EditPage />} />
     <Route path="/diary/:id" element={<DiaryPage />} />
+    <Route path="*" element={<Navigate to="/" replace />} />
   </Routes>
 );
 

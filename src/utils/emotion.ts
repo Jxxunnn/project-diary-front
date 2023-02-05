7c37fa57cbf6d5b987c09a83758593ae91e3f985
Jxxunnn <roof2586@naver.com>
2023-02-05 22:11:22 +0900

    feat 감정 데이터 구조화

---
+++ src/utils/emotion.ts
@@ -0,0 +1,33 @@
+import happyEmotionImage from "../assets/images/emotion1.png";
+import goodEmotionImage from "../assets/images/emotion2.png";
+import sosoEmotionImage from "../assets/images/emotion3.png";
+import badEmotionImage from "../assets/images/emotion4.png";
+import angryEmotionImage from "../assets/images/emotion5.png";
+
+export const emotionList = [
+  {
+    emotion_id: 1,
+    emotion_img: happyEmotionImage,
+    emotion_descript: "완전 좋음",
+  },
+  {
+    emotion_id: 2,
+    emotion_img: goodEmotionImage,
+    emotion_descript: "좋음",
+  },
+  {
+    emotion_id: 3,
+    emotion_img: sosoEmotionImage,
+    emotion_descript: "그럭저럭",
+  },
+  {
+    emotion_id: 4,
+    emotion_img: badEmotionImage,
+    emotion_descript: "나쁨",
+  },
+  {
+    emotion_id: 5,
+    emotion_img: angryEmotionImage,
+    emotion_descript: "끔찍함",
+  },
+];

c25fd10bc8e6b6833d6d2723f78883e51d512d5c
Jxxunnn <roof2586@naver.com>
2023-02-05 22:15:46 +0900

    feat 페이지 자식 컴포넌트 변경

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
+import Home from "../../components/Home";
 
-const HomePage = () => <div>HomePage</div>;
+const HomePage = () => <Home />;
 
 export default HomePage;
--- src/pages/New/index.tsx
+++ src/pages/New/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
+import New from "../../components/New";
 
-const NewPage = () => <div>NewPage</div>;
+const NewPage = () => <New />;
 
 export default NewPage;
